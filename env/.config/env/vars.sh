fb40dcbb8351ab4fca74ab0c5efe50f8b81fb9d1
Michael Dunleavy <mike@dunleavy.ie>
2020-02-03 13:09:55 +0000

    Description of Wayland environment setup

--- env/.config/env/vars.sh
+++ env/.config/env/vars.sh
@@ -1,24 +1,3 @@
-#
-# Time for a rant about environment variables. There is no portable way to set
-# environment variables in GNU/Linux, yet alone POSIX. There is no single file
-# that is guaranteed to work on all distros, shells, and display managers.
-#
-# ~/.config/environment.d/*.conf is systemd exclusive.
-#
-# ~/.pam-environment looked promising but I found it wasn't read by default on
-# Debian. (Either that or I misspelled environment.)
-#
-# ~/.profile tends to get ignored when more specific alternatives, such as a
-# ~/.bashrc, are left on the system. Also GDM ignores ~/.profile for Wayland
-# sessions.
-#
-# I've chosen to use ~/.profile because it's a shell script unlike the other
-# options. This makes it more versatile and more portable. I use GDM and Wayland
-# on my laptop. My solution is to modify the desktop entries in
-# /usr/share/wayland-sessions/ to source ~/.profile before running the
-# compositor.
-#
-
 # C-header style lock to prevent file being sourced more than once
 if [ -z "$ENVVAR_LOCK" ]; then
 export ENVVAR_LOCK=1
