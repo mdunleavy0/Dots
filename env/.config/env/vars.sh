e51da782b5b7fbc3fb8290cb84f4e29b5c1aacad
Michael Dunleavy <mike@dunleavy.ie>
2023-08-02 21:56:31 +0100

    Add the new XDG user state directory

--- env/.config/env/vars.sh
+++ env/.config/env/vars.sh
@@ -11,6 +11,7 @@ export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
 
 ################################################################################
 
